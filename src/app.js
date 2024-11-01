9d7c15a35dec9957cb7f9ecac201105bff966775
Anil05/10 <anilmunna623@gmail.com>
2024-11-01 14:03:02 +0530

    exploring get,post ,delete

--- src/app.js
+++ src/app.js
@@ -1,8 +1,26 @@
 const express=require('express');
 
 const app= express();
+// this will only call handle  get call to /user
+app.get("/user",(req,res)=>{
+    res.send({
+        firstName:'anil',
+        secondName:'d',
+        location:'Hyderbad'
+    })
+
+})
+app.post((req,res)=>{
+    res.send("data save from database sucessfully")
+
+})
+app.delete((req,res)=>{
+    res.send("Deleted sucessfulyy")
+
+})
+// this method will match all the HTTP methods API call to /test
 app.use("/helo",(req,res)=>{
-    res.send("hello hello")
+    res.send("hello hello")// if we give /  insted of /hello routing it will over ride
 
 })
 app.use("/test",(req,res)=>{
